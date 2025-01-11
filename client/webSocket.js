3c51a7d4b06ec0fe8b2248f5fefa28debbc22729
alirezaraissattari <alirezaraissattari@gmail.com>
2025-01-12 00:17:06 +0330

    fix

--- client/webSocket.js
+++ client/webSocket.js
@@ -38,8 +38,9 @@ const createHost = (wss, eventHandlers) =>
       console.log("New WebSocket connection");
 
       ws.on("message", (message) => {
-        console.log(`received event as host: ${message}`);
-        handleReceivedMessages(message, eventHandlers);
+        const parsedMsg = JSON.parse(message);
+        console.log(`received event as host: ${parsedMsg}`);
+        handleReceivedMessages(parsedMsg, eventHandlers);
       });
       ws.on("error", (message) => {
         reject(message);
