f5d8cd467d1ac153406130de724b3e68812557cb
AmirHossienAfshar <130398301+AmirHossienAfshar@users.noreply.github.com>
2025-01-11 22:59:20 +0330

    addressing is improved

--- pynodeqml2/main.py
+++ pynodeqml2/main.py
@@ -2,9 +2,15 @@ import sys
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType, qmlRegisterModule
 from pyside_client import user_register
-
+import os
 
 if __name__ == "__main__":
+    
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
+
+    # Construct the full path to the QML file
+    qml_file = os.path.join(script_dir, "wellcome_page.qml")
     
     qmlRegisterType(user_register, "User_registeration", 1, 0, "User_registration_class")
     
