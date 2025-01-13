d6c2a5bacddbba08374e8e7eb45dbfe3e1e0605f
AmirHossienAfshar <130398301+AmirHossienAfshar@users.noreply.github.com>
2025-01-13 17:03:06 +0330

    back to flask logic

--- pynodeqml2/server_flask.py
+++ pynodeqml2/server_flask.py
@@ -3,9 +3,9 @@ from flask_socketio import SocketIO, emit
 import threading
 
 class SocketIOServer:
-    def __init__(self, host='127.0.0.1', port=5000, secret_key='secret!', debug=True):
+    def __init__(self, host='127.0.0.1', port=5001, secret_key='secret!', debug=True):
         self.app = Flask(__name__)
-        self.app.config['SECRET_KEY'] = secret_key
+        # self.app.config['SECRET_KEY'] = secret_key
         self.socketio = SocketIO(self.app)
         self.host = host
         self.port = port
@@ -26,7 +26,9 @@ class SocketIOServer:
         print(f"[SERVER] Received data from PySide: {data}")
         self.received_data = data
         self.data_received_event.set()  # Signal that the data has been received
-        
+        # emit('message_from_python', {
+        #     'data': f"Python received:////////////////////////////////////////////////"
+        #     })
 
     def _register_event_handlers(self):
         """
@@ -88,3 +90,82 @@ class SocketIOServer:
 
 #     # Start the server
 #     server.start()
+
+
+# import socketio
+# import eventlet
+# import threading
+
+
+# class SocketIOServer:
+#     def __init__(self, host='127.0.0.1', port=5001, debug=True):
+#         # Initialize the Socket.IO server with CORS support
+#         self.sio = socketio.Server(logger=debug, engineio_logger=debug, cors_allowed_origins='*')
+#         self.app = socketio.WSGIApp(self.sio)  # WSGI wrapper for the server
+
+#         self.host = host
+#         self.port = port
+#         self.debug = debug
+
+#         # Shared data
+#         self.received_data = None
+
+#         # Register event handlers
+#         self._register_event_handlers()
+
+#     def _register_event_handlers(self):
+#         """
+#         Register Socket.IO event handlers for client communication.
+#         """
+#         @self.sio.event
+#         def connect(sid, environ):
+#             print(f"Client connected: {sid}")
+#             # Optionally, send a welcome message to the newly connected client
+#             self.sio.emit('message_from_python', {'data': 'Hello from Python!'}, to=sid)
+
+#         @self.sio.on('message_from_node')
+#         def handle_message_from_node(sid, data):
+#             """
+#             Handle messages sent from Node.js.
+#             """
+#             print(f"[SERVER] Received from Node: {data}")
+#             # Example response to Node.js
+#             self.sio.emit('message_from_python', {
+#                 'data': f"Python received your message: {data['data']}"
+#             }, to=sid)
+
+#     def receive_data_from_pyside(self, data):
+#         """
+#         Method to receive data from PySide and immediately emit it to Node.js.
+#         """
+#         print(f"[SERVER] Received data from PySide: {data}")
+#         self.received_data = data
+
+#         # Emit the data directly to all connected clients
+#         self.sio.emit('message_from_python', {
+#             'data': f"New data from PySide: {data}"
+#         })
+#         print(f"[SERVER] Emitted data to Node.js: {data}")
+
+#     def start(self):
+#         """
+#         Start the Socket.IO server using Eventlet.
+#         """
+#         print(f"Starting Socket.IO server on {self.host}:{self.port}...")
+#         # Run the WSGI server with the Socket.IO app
+#         eventlet.wsgi.server(eventlet.listen((self.host, self.port)), self.app)
+
+
+# # Example usage
+# # if __name__ == '__main__':
+# #     # Create the server instance
+# #     server = SocketIOServer(host='127.0.0.1', port=5001, debug=True)
+
+# #     # Start the server in a new thread (optional, if you want the main thread to do other things)
+# #     threading.Thread(target=server.start, daemon=True).start()
+
+# #     # Example interaction with PySide:
+# #     while True:
+# #         # Simulate receiving data from PySide
+# #         input_data = input("Enter data to send to Node.js: ")
+# #         server.receive_data_from_pyside(input_data)
