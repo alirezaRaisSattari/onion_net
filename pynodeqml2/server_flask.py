06c23231b735495c8ece6fb0e141456d8ea106f7
AmirHossienAfshar <130398301+AmirHossienAfshar@users.noreply.github.com>
2025-01-14 23:57:01 +0330

    changes

--- pynodeqml2/server_flask.py
+++ pynodeqml2/server_flask.py
@@ -43,33 +43,37 @@ class SocketIOServer:
         def handle_message_from_node(json_data):
             print('Received from Node:', json_data)
             
-            if json_data['data'] == "username":
-                # Wait until data is received from PySide
-                print("[FLASK] username condition is triggerd!")
-                if not self.data_received_event.is_set():
-                    emit('message_from_python', {
-                        'data': "Waiting for data from PySide..."
-                    })
-                    print("Waiting for data from PySide...")
-                    self.data_received_event.wait()
+            # if json_data['data'] == "username":
+            #     # Wait until data is received from PySide
+            #     print("[FLASK] username condition is triggerd!")
+            #     if not self.data_received_event.is_set():
+            #         emit('message_from_python', {
+            #             'data': "Waiting for data from PySide..."
+            #         })
+            #         print("Waiting for data from PySide...")
+            #         self.data_received_event.wait()
                     
-                emit('message_from_python', {
-                    'data': f"Python received: {json_data['data']} and PySide provided: {self.received_data}"
+                # emit('message_from_python', {
+                #         'data': f"Python received: {json_data['data']} and PySide provided: {self.received_data}"
+                #         })
+                
+            emit('message_from_python', {
+                    'data': f"Python received: this is the client2 that got the message"
                     })
             
-            elif json_data['data'] == "userlist":
-                print("[FLASK] userlist condition is triggerd!")
-                if not self.data_received_event.is_set():
-                    emit('message_from_python', {
-                        'data': "Waiting for data from PySide..."
-                    })
-                    print("Waiting for data from PySide...")
-                    self.data_received_event.wait()
+            # elif json_data['data'] == "userlist":
+            #     print("[FLASK] userlist condition is triggerd!")
+            #     if not self.data_received_event.is_set():
+            #         emit('message_from_python', {
+            #             'data': "Waiting for data from PySide..."
+            #         })
+            #         print("Waiting for data from PySide...")
+            #         self.data_received_event.wait()
                     
-                emit('message_from_python', {
-                    'data': f"Python received: {json_data['data']} and PySide provided: {self.received_data}"
-                    })
-            # Respond to the Node client
+                # emit('message_from_python', {
+                #         'data': f"Python received: {json_data['data']} and PySide provided: {self.received_data}"
+                #         })
+                # # Respond to the Node client
             
 
 
